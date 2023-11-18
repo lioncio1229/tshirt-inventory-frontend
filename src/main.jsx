8037ec1183a307b852f3c4c5bdc51fe6d0c6f2df
Lioncio Morcilla <lionciomorcilla12@gmail.com>
2023-11-18 20:28:26 +0800

    Validation

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { RouterProvider } from "react-router-dom";
 import router from './pages/router'
+import { SnackbarProvider } from 'notistack';
 
 const defaultTheme = createTheme();
 
@@ -13,8 +14,10 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
       <Provider store={store}>
         <ThemeProvider theme={defaultTheme}>
-          <CssBaseline />
-          <RouterProvider router={router} />
+          <SnackbarProvider>
+            <CssBaseline />
+            <RouterProvider router={router} />
+          </SnackbarProvider>
         </ThemeProvider>
       </Provider>
   </React.StrictMode>,
--- src/pages/auth/Registration.jsx
+++ src/pages/auth/Registration.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState, useMemo } from "react";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
@@ -14,13 +15,38 @@ export default function Registration() {
   const [register] = useRegisterMutation();
   const navigate = useNavigate();
 
+  const [error, setError] = useState({
+    emailError: "",
+    fullNameError: "",
+    passwordError: "",
+    confirmPasswordError: "",
+  });
+
+  const [errorState, setErrorState] = useState({
+    emailHasError: true,
+    fullNameHasError: true,
+    passwordHasError: true,
+    confirmPasswordHasError: true,
+  });
+  
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const isEnabled = useMemo(() => Object.values(errorState).every(item => !item), [errorState]);
+  
   const handleSubmit = (event) => {
     event.preventDefault();
+
     const data = new FormData(event.currentTarget);
+    const email = data.get("email").trim();
+    const fullName = data.get("fullName").trim();
+    const password = data.get("password");
+    const confirmPassword = data.get("confirmPassword");
+
     const model = {
-      email: data.get("email"),
-      fullName: data.get("fullName"),
-      password: data.get("password")
+      email,
+      fullName,
+      password,
     };
 
     register(model).unwrap().then(resp => {
@@ -28,6 +54,92 @@ export default function Registration() {
       navigate("/main");
     });
   };
+
+  const handleEmailInputChange = (e) => {
+    const { value } = e.target;
+    const email = value.trim();
+    let errorValue = "";
+    let hasError = false;
+
+    if(email.trim() === "")
+    {
+      errorValue = "Please add email";
+      hasError = true;
+    }
+    else if(!(/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)))
+    {
+      errorValue = "Invalid email";
+      hasError = true;
+    }
+
+    setError({...error, emailError: errorValue});
+    setErrorState({...errorState, emailHasError: hasError});
+  }
+
+  const handleFullNameInputChange = (e) => {
+    const { value } = e.target;
+    const fullName = value.trim();
+    let errorValue = "";
+    let hasError = false;
+
+    if(fullName === "") {
+      errorValue = "Please add your full name";
+      hasError = true;
+    }
+
+    setError({...error, fullNameError: errorValue});
+    setErrorState({...errorState, fullNameHasError: hasError});
+  }
+
+  const handlePasswordInputChange = (e) => {
+    const { value: password } = e.target;
+
+    let errorValue = "";
+    let hasError = false;
+
+    if(password.length < 6){
+      errorValue = "Password characters minimum of 6";
+      hasError = true;
+    }
+
+    setError((error) => ({...error, passwordError: errorValue}));
+    setErrorState({...errorState, passwordHasError: hasError});
+    setPassword(password);
+  }
+  
+  const handleConfirmPasswordInputChange = (e) => {
+    const { value: confirmPassword } = e.target;
+
+    let errorValue = "";
+    let hasError = false;
+
+    if(confirmPassword !== password){
+      errorValue = "Password not matched";
+      hasError = true;
+    }
+
+    setError((error) => ({...error, confirmPasswordError: errorValue}));
+    setErrorState({...errorState, confirmPasswordHasError: hasError});
+    setConfirmPassword(confirmPassword);
+  }
+
+  useEffect(() => {
+    let errorValue = "";
+    let hasError = false;
+
+    if(confirmPassword.length > 0 && password !== confirmPassword)
+    {
+      errorValue = "Password not matched";
+    }
+    if(password !== confirmPassword)
+    {
+      hasError = true;
+    }
+
+    setError((error) => ({...error, confirmPasswordError: errorValue}));
+    setErrorState({...errorState, confirmPasswordHasError: hasError});
+
+  }, [password]);
 
   return (
     <Box
@@ -53,6 +165,9 @@ export default function Registration() {
               label="Email Address"
               name="email"
               autoComplete="email"
+              error={error.emailError.length > 0}
+              helperText={error.emailError}
+              onChange={handleEmailInputChange}
             />
           </Grid>
           <Grid item xs={12}>
@@ -64,6 +179,9 @@ export default function Registration() {
               id="fullName"
               label="Full Name"
               autoFocus
+              error={error.fullNameError.length > 0}
+              helperText={error.fullNameError}
+              onChange={handleFullNameInputChange}
             />
           </Grid>
           <Grid item xs={12}>
@@ -75,6 +193,9 @@ export default function Registration() {
               type="password"
               id="password"
               autoComplete="new-password"
+              error={error.passwordError.length > 0}
+              helperText={error.passwordError}
+              onChange={handlePasswordInputChange}
             />
           </Grid>
           <Grid item xs={12}>
@@ -85,6 +206,9 @@ export default function Registration() {
               label="Confirm Password"
               type="password"
               id="confirmPassword"
+              error={error.confirmPasswordError.length > 0}
+              helperText={error.confirmPasswordError}
+              onChange={handleConfirmPasswordInputChange}
             />
           </Grid>
         </Grid>
@@ -93,6 +217,7 @@ export default function Registration() {
           fullWidth
           variant="contained"
           sx={{ mt: 3, mb: 2 }}
+          disabled={!isEnabled}
         >
           Register
         </Button>
