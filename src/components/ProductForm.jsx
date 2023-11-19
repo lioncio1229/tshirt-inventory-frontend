ed8094768153afe5b64d9afda9e95bb6226ef607
Lioncio Morcilla <lionciomorcilla12@gmail.com>
2023-11-19 20:12:13 +0800

    Improve image uploader looks

--- src/components/ProductForm.jsx
+++ src/components/ProductForm.jsx
@@ -8,6 +8,7 @@ import {
   Stack,
 } from "@mui/material";
 import LabeledInputfield from "./LabeledInputField";
+import GalleryIcon from "../assets/gallery.png";
 
 export default function ProductForm({
   title,
@@ -21,27 +22,48 @@ export default function ProductForm({
   return (
     <Grid container spacing={2} columnSpacing={4}>
       <Grid item xs={12}>
-        <Typography variant="h5" color="secondary">
+        <Typography variant="h5" color="primary">
           {title}
         </Typography>
       </Grid>
       <Grid item xs={6} md={3}>
         <Box
           sx={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            bgcolor: "lightgrey",
+            bgcolor: "grey.300",
             height: { xs: "40vw", md: "21vw" },
             cursor: "pointer",
             transition: "background-color 0.5s",
-            "&:hover": {
-              bgcolor: "grey",
-            },
+            p: 2,
           }}
         >
-          <img src="" />
-          <Typography>Click to add image</Typography>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              flexDirection: "column",
+              width: "100%",
+              height: "100%",
+              backgroundImage: `url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' rx='15' ry='15' stroke='white' stroke-width='5' stroke-dasharray='6%2c 15' stroke-dashoffset='0' stroke-linecap='square'/%3e%3c/svg%3e");
+            border-radius: 15px;`,
+
+              "&:hover img": {
+                transform: "scale(1.12)",
+              },
+            }}
+          >
+            <Box
+              component="img"
+              src={GalleryIcon}
+              sx={{
+                width: "27%",
+                height: "auto",
+                mb: 2,
+                transition: "transform 0.5s",
+              }}
+            />
+            <Typography>Click to add image</Typography>
+          </Box>
         </Box>
       </Grid>
       <Grid item xs={12} md={9}>
@@ -98,7 +120,7 @@ export default function ProductForm({
           </Grid>
           <Grid item xs={12} md={6}>
             <Autocomplete
-                value={values.category}
+              value={values.category}
               isOptionEqualToValue={(option) =>
                 option.id === values.category.id
               }
@@ -123,7 +145,7 @@ export default function ProductForm({
             <Stack flexDirection="row" gap={2}>
               <Button
                 variant="contained"
-                color="success"
+                color="secondary"
                 sx={{ minWidth: 130, textTransform: "capitalize" }}
                 onClick={onSubmit}
               >
